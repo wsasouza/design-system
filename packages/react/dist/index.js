7cc0b047f55113e3099bbbbdcbe4302705bca211
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-11-12 12:45:12 -0300

    Feat: Create TextArea Component

--- packages/react/dist/index.js
+++ packages/react/dist/index.js
@@ -31,6 +31,7 @@ __export(src_exports, {
   Button: () => Button,
   Heading: () => Heading,
   Text: () => Text,
+  TextArea: () => TextArea,
   TextInput: () => TextInput
 });
 module.exports = __toCommonJS(src_exports);
@@ -391,6 +392,32 @@ function TextInput({ prefix, ...props }) {
     ]
   });
 }
+
+// src/components/TextArea.tsx
+var TextArea = styled("textarea", {
+  backgroundColor: "$gray900",
+  padding: "$3 $4",
+  borderRadius: "$sm",
+  boxSizing: "border-box",
+  border: "2px solid $gray900",
+  fontFamily: "$default",
+  fontSize: "$sm",
+  color: "$white",
+  fontWeight: "$regular",
+  resize: "vertical",
+  minHeight: 80,
+  "&:focus": {
+    outline: 0,
+    borderColor: "$blue350"
+  },
+  "&:disabled": {
+    opacity: 0.5,
+    cursor: "not-allowed"
+  },
+  "&:placeholder": {
+    color: "$gray400"
+  }
+});
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   Avatar,
@@ -398,5 +425,6 @@ function TextInput({ prefix, ...props }) {
   Button,
   Heading,
   Text,
+  TextArea,
   TextInput
 });
